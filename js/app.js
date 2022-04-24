2dcafaf07aa92c8522d8bd5fa87078fe3faac3b1
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-04-24 20:06:12 +0300

    add comments for  script

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 // 3D Scroll
 
-let zSpacing = -1000,
-		lastPos = zSpacing / 5,
+let zSpacing = -1000, //Расстояние по оси Z
+		lastPos = zSpacing / 5, //Стартовая позиция каждого фрейма
 		$frames = document.getElementsByClassName('frame'),
-		frames = Array.from($frames),
+		frames = Array.from($frames), //Массив из всех элементов
 		zVals = []
 
 window.onscroll = function() {
 
-	let top = document.documentElement.scrollTop,
-			delta = lastPos - top
+	let top = document.documentElement.scrollTop, //Расстояние от верха до текущей позиции при скролле
+			delta = lastPos - top //Вычисляет последнюю позицию
 
-	lastPos = top
+	lastPos = top //Обновляем последнюю позицию
 
-	frames.forEach(function(n, i) {
+	frames.forEach(function(n, i) { //n - это текущий обрабатываемый элемент, а i - это индекс его
 		zVals.push((i * zSpacing) + zSpacing)
 		zVals[i] += delta * -5.5
 		let frame = frames[i],
